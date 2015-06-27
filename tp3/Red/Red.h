2257fea3c27b1c296810b928816a2510fe6ee336
Leandro <leandrovigali@yahoo.com.ar>
2015-06-27 19:49:30 -0300

    fix Red

--- tp3/Red/Red.h
+++ tp3/Red/Red.h
@@ -4,6 +4,17 @@
 
 typedef String hostname;
 typedef Nat interfaz; 
+
+struct computadora{
+	hostname ip;
+	Conj<interfaz> cinterfaces;
+
+	computadora(hostname ipnueva, Conj<interfaz> cji){
+		ip = ipnueva;
+		cinterfaces = cji;
+	}
+};
+
 class Red{
 	
 	private:
@@ -11,9 +22,9 @@ class Red{
 		struct Datos{
 			Conj<interfaz> interfaces;
 			Dicc<interfaz, hostname> conexiones;
-			Dicc<hostname, Conj<Lista(hostname)> > alcanzables;
+			Dicc<hostname, Conj< Lista<hostname> > > alcanzables;
 
-			Datos(Conj<interfaz> is, Dicc<interfaz, hostname> cs, Dicc<hostname, Conj<Lista(hostname)> > as){
+			Datos(Conj<interfaz> is, Dicc<interfaz, hostname> cs, Dicc<hostname, Conj< Lista<hostname> > > as){
 				interfaces = is;
 				conexiones = cs;
 				alcanzables = as;
@@ -25,30 +36,58 @@ class Red{
 	public:
 		
 
-		Conj<hostname> Computadoras(const Red redi);
-		bool Conectadas(const Red redi, const hostname c1, const hostname c2);
-		interfaz InterfazUsada(const Red redi, const hostname c1, const hostname c2);
+		Conj<hostname> Computadoras();
+		bool Conectadas(const hostname c1, const hostname c2);
+		interfaz InterfazUsada(const hostname c1, const hostname c2);
 		Red IniciarRed();
-		void AgregarCompu(Red redi, const );
-		Conectar(Red redi, const hostname c1, int i1, const hostname c2, const int i2);
-		Conj<hostname> Vecinos(const Red redi, const hostname);
-		bool UsaInterfaz(const Red redi, const hostname c, const interfaz i);
-		Conj< Lista<hostname> > CaminosMinimos(const Red redi, const hostname c1, const hostname c2);
-		bool HayCamino(const Red redi, const hostname c1, const hostname c2);
+		void AgregarCompu(const computadora compu);
+		void Conectar(const hostname c1, interfaz i1, const hostname c2, const interfaz i2);
+		Conj<hostname> Vecinos(const hostname);
+		bool UsaInterfaz(const hostname c, const interfaz i);
+		Conj< Lista<hostname> > CaminosMinimos(const hostname c1, const hostname c2);
+		bool HayCamino(const hostname c1, const hostname c2);
 		bool operator == (const Red redi) const;
-		Copiar
+		//Falta Copiar
 
 
 };
 
 
-Conj<hostname> Computadoras(const Red redi){
-	typename  Red::const_Iterador it = redi.CrearIt();
+Conj<hostname> Red::Computadoras(){
+	typename  Dicc<hostname, Red::Datos>::const_Iterador it = red_.CrearIt();
 	Conj<hostname> conjcompus;
 	while(it.HaySiguiente()){
-		conjcompus.AgregarRapido(it.Siguiente());
+		conjcompus.AgregarRapido(it.SiguienteClave());
 		it.Avanzar();
 	}
 	return conjcompus;
 }
 
+bool Red::Conectadas(const hostname c1, const hostname c2){
+	typename Dicc<interfaz, hostname>::const_Iterador it = red_.Significado(c1).conexiones.CrearIt();
+	bool res = false;
+	while(it.HaySiguiente() && (!res)){
+		if(it.SiguienteSignificado() == c2){
+			res = true;
+		}
+		it.Avanzar();
+	}
+	return res;
+}
+
+
+
+
+interfaz InterfazUsada(const hostname c1, const hostname c2);
+Red IniciarRed();
+
+void AgregarCompu(Red redi, const computadora compu){
+
+}
+
+void Conectar(Red redi, const hostname c1, interfaz i1, const hostname c2, const interfaz i2);
+Conj<hostname> Vecinos(const hostname);
+bool UsaInterfaz(const hostname c, const interfaz i);
+Conj< Lista<hostname> > CaminosMinimos(const hostname c1, const hostname c2);
+bool HayCamino(const hostname c1, const hostname c2);
+bool operator == (const Red red1, const Red red2);
