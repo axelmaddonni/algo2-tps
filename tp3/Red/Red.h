01b606549da89f4471258ff89934105aa472b97f
Axel Maddonni <axel.maddonni@gmail.com>
2015-07-01 18:16:09 -0300

    red, modificada, con algunos tests, prueben con mas ejemplos que se les ocurran, cambie un poco ActualizarVecinos

--- tp3/Red/Red.h
+++ tp3/Red/Red.h
@@ -52,6 +52,7 @@ class Red{
 		bool operator == (const Red& otrared) const;
 
 		//IMPRIMIR RED (para testear)
+		std::ostream& ImprimirRed(std::ostream& os) const;
 };
 
 
@@ -117,16 +118,102 @@ void Red::Conectar(const hostname c1, interfaz i1, const hostname c2, const inte
 	//actualizo conexiones de ambas
 	red_.Significado(c1).conexiones.DefinirRapido(i1, c2);
 	red_.Significado(c2).conexiones.DefinirRapido(i2, c1);
-	//actualizo caminos de ambas
+	//actualizo el camino de c1 a c2
+	Conj< Lista<hostname> > nc1 = Conj< Lista<hostname> >();
+	Lista<hostname> nl1 = Lista<hostname>();
+	nl1.AgregarAtras(c2);
+	nc1.AgregarRapido(nl1);
+	red_.Significado(c1).alcanzables.Definir(c2, nc1);
+	//actualizo el camino de c2 a c1
+	Conj< Lista<hostname> > nc2 = Conj< Lista<hostname> >();
+	Lista<hostname> nl2 = Lista<hostname>();
+	nl2.AgregarAtras(c1);
+	nc2.AgregarRapido(nl2);
+	red_.Significado(c2).alcanzables.Definir(c1, nc2);
+	//actualizo otros caminos de ambas 
 	this->ActualizarCaminos(c1, c2);
 	this->ActualizarCaminos(c2, c1);
+
+	//actualizo los caminos del resto la red
+	Conj<hostname> actualizados = Conj<hostname>();
+	this->ActualizarVecinos(c1, actualizados);
+
+/*	-----------------------------------------------------------------------
+
+	usando un while con iteradores de la red :
+
+	typename Dicc<hostname, Datos>::const_Iterador itRed = red_.CrearIt();
+	
+	while ( actualizados.Cardinal() != red_.CantClaves() ){
+		if (!actualizados.Pertenece(itRed.SiguienteClave()) ){
+			actualizados.AgregarRapido(itRed.SiguienteClave());
+			ActualizarVecinos(itRed.SiguienteClave(), actualizados);
+		}
+		itRed.Avanzar();
+	}
+
+	------------------------------------------------------------------------
+
+	usando recursion: (version 1)
+
 	//creo conjuntos con los actualizados hasta el momento
 	Conj<hostname> actualizados = Conj<hostname>();
 	actualizados.AgregarRapido(c1);
 	actualizados.AgregarRapido(c2);
+	Conj<hostname> vecinosActualizados = Conj<hostname>();
+	vecinosActualizados.AgregarRapido(c2);
 	//actualizo caminos del resto de la red por recursion
-	this->ActualizarVecinos(c1, actualizados);
+	this->ActualizarVecinos(c1, actualizadosGlobal, vecinosActualizados);
+
+*/
+
 }
+
+//funcion private
+void Red::ActualizarVecinos(const hostname c1, Conj<hostname>& actualizados){
+	
+	typename Dicc<interfaz, hostname>::Iterador itVecinos = red_.Significado(c1).conexiones.CrearIt();
+	while(itVecinos.HaySiguiente()){
+		//Si todavía no fue actualizado, lo actualizo
+		if ( !actualizados.Pertenece(itVecinos.SiguienteSignificado()) ){
+			this->ActualizarCaminos(c1, itVecinos.SiguienteSignificado());
+			this->ActualizarCaminos(itVecinos.SiguienteSignificado(), c1);
+		}
+		itVecinos.Avanzar();
+	}
+	actualizados.Agregar(c1);
+
+	itVecinos = red_.Significado(c1).conexiones.CrearIt();
+	while(itVecinos.HaySiguiente()){
+		//recursion sobre los vecinos que no habian sido actualizados
+		if ( !actualizados.Pertenece(itVecinos.SiguienteSignificado()) ){
+			this->ActualizarVecinos(itVecinos.SiguienteSignificado(), actualizados);
+		}
+		itVecinos.Avanzar();
+	}
+}
+
+/*
+//funcion private
+void Red::ActualizarVecinos(const hostname c1, Conj<hostname>& actualizadosGlobal, Conj<hostname>& vecinosActualizados){
+	//Actualiza los caminos de los vecinos de C, y luego hace recursión para los vecinos de los vecinos.
+	typename Dicc<interfaz, hostname>::Iterador itVecinos = red_.Significado(c1).conexiones.CrearIt();
+	actualizadosGlobal.Agregar(c1);
+	while(itVecinos.HaySiguiente()){
+		//Si todavía no fue actualizado, lo actualizo y hago recursión sobre los vecinos
+		if ( !actualizadosGlobal.Pertenece(itVecinos.SiguienteSignificado()) && 
+			 !vecinosActualizados.Pertenece(itVecinos.SiguienteSignificado()) ){
+			this->ActualizarCaminos(c1, itVecinos.SiguienteSignificado());
+			this->ActualizarCaminos(itVecinos.SiguienteSignificado(), c1);
+			vecinosActualizados.AgregarRapido(itVecinos.SiguienteSignificado());
+			Conj<hostname> vecinosNuevos = Conj<hostname>();
+			this->ActualizarVecinos(itVecinos.SiguienteSignificado(), actualizadosGlobal, vecinosNuevos);
+		}
+		itVecinos.Avanzar();
+	}
+	
+}
+*/
 
 //funcion private
 void Red::ActualizarCaminos(const hostname c1, const hostname c2){
@@ -137,20 +224,23 @@ void Red::ActualizarCaminos(const hostname c1, const hostname c2){
 	Lista<hostname> camino2;
 	Lista<hostname> nuevoCamino;
 
+	bool ready = false;
+
 	//Actualiza caminos de c1 con los de c2
 	//recorro alcanzables de c2
 	typename Dicc<hostname, Conj< Lista<hostname> > >::Iterador itAlcanzables2 = red_.Significado(c2).alcanzables.CrearIt();
 	while(itAlcanzables2.HaySiguiente()){
 		//recorro alcanzables de c1
 		typename Dicc<hostname, Conj< Lista<hostname> > >::Iterador itAlcanzables1 = red_.Significado(c1).alcanzables.CrearIt();
-		while(itAlcanzables1.HaySiguiente()){
+		while(itAlcanzables1.HaySiguiente() && !ready){
 			if(itAlcanzables2.SiguienteClave() == itAlcanzables1.SiguienteClave()){
 				//el alcanzable ya estaba, me fijo que caminos son más cortos
+				ready = true;
 				itCaminos = itAlcanzables2.SiguienteSignificado().CrearIt();
 				camino2 = itCaminos.Siguiente();
 				itCaminos = itAlcanzables1.SiguienteSignificado().CrearIt();
 				camino1 = itCaminos.Siguiente();
-				if (camino1.Longitud() > camino2.Longitud()){
+				if (camino1.Longitud() > camino2.Longitud() + 1){
 					//los caminos nuevos son más cortos, borro los que ya están y copio los nuevos
 					red_.Significado(c1).alcanzables.Borrar(itAlcanzables1.SiguienteClave());
 					//nuevo alcanzable: me copio los caminos agregando c1 al principio
@@ -158,56 +248,45 @@ void Red::ActualizarCaminos(const hostname c1, const hostname c2){
 					caminos = Conj< Lista<hostname> >(); //vacio
 					while (itCaminos.HaySiguiente()){
 						nuevoCamino = Lista<hostname>(itCaminos.Siguiente());
-						nuevoCamino.AgregarAdelante(c1);
+						nuevoCamino.AgregarAdelante(c2);
 						caminos.AgregarRapido(nuevoCamino);
 						itCaminos.Avanzar(); 
 					}
 					//agrego el nuevo alcanzable con el camino
-					red_.Significado(c1).alcanzables.DefinirRapido(itAlcanzables2.SiguienteClave(), caminos);					
+					red_.Significado(c1).alcanzables.Definir(itAlcanzables2.SiguienteClave(), caminos);			
 				}else{
-					if (camino1.Longitud() == camino2.Longitud()){
+					if (camino1.Longitud() == camino2.Longitud() + 1){
 						//tengo que agregar los nuevos caminos(modificados) al conjunto de caminos actual
 						itCaminos = itAlcanzables2.SiguienteSignificado().CrearIt();
 						while(itCaminos.HaySiguiente()){
 							nuevoCamino = Lista<hostname>(itCaminos.Siguiente());
-							nuevoCamino.AgregarAdelante(c1);
+							nuevoCamino.AgregarAdelante(c2);
 							itAlcanzables1.SiguienteSignificado().Agregar(nuevoCamino);
 							itCaminos.Avanzar();
 						}
 					}
 				}
-			}else{
-				//Nuevo alcanzable: me copio los caminos agregando c1 al principio
+			}
+			itAlcanzables1.Avanzar();
+		}
+			if (!ready && itAlcanzables2.SiguienteClave() != c1){
+				//Nuevo alcanzable: me copio los caminos agregando c2 al principio
 				itCaminos = itAlcanzables2.SiguienteSignificado().CrearIt();
 				caminos = Conj< Lista<hostname> >(); //vacio
 				while(itCaminos.HaySiguiente()){
 					nuevoCamino = Lista<hostname>(itCaminos.Siguiente());
-					nuevoCamino.AgregarAdelante(c1);
+					nuevoCamino.AgregarAdelante(c2);
 					caminos.AgregarRapido(nuevoCamino);
 					itCaminos.Avanzar();
 				}
 				red_.Significado(c1).alcanzables.DefinirRapido(itAlcanzables2.SiguienteClave(), caminos);
 			}
-			itAlcanzables1.Avanzar();
-		}
+		ready = false;	
 		itAlcanzables2.Avanzar();
 	}
 }
 
-//funcion private
-void Red::ActualizarVecinos(const hostname c1, Conj<hostname>& actualizados){
-	//Actualiza los caminos de los vecinos de C, y luego hace recursión para los vecinos de los vecinos.
-	typename Dicc<interfaz, hostname>::Iterador itVecinos = red_.Significado(c1).conexiones.CrearIt();
-	while(itVecinos.HaySiguiente()){
-		//Si todavía no fue actualizado, lo actualizo y hago recursión sobre los vecinos
-		if ( !actualizados.Pertenece(itVecinos.SiguienteSignificado()) ){
-			this->ActualizarCaminos(itVecinos.SiguienteSignificado(), c1);
-			actualizados.AgregarRapido(itVecinos.SiguienteSignificado());
-			this->ActualizarVecinos(itVecinos.SiguienteSignificado(), actualizados);
-		}
-		itVecinos.Avanzar();
-	}
-}
+
 
 Conj<hostname> Red::Vecinos(const hostname compu) const{
 	typename Dicc<interfaz, hostname>::const_Iterador it = red_.Significado(compu).conexiones.CrearIt();
@@ -241,5 +320,37 @@ bool Red::HayCamino(const hostname c1, const hostname c2) const{
 
 bool Red::operator == (const Red& otrared) const{
 		//+++ COMPLETAR +++
+}
+
+std::ostream& Red::ImprimirRed(std::ostream& os) const{
+	typename Dicc<hostname, Datos>::const_Iterador itRed = red_.CrearIt();
+	os << "Información de la Red:" << std::endl;
+	while (itRed.HaySiguiente()){
+		
+		os << "Hostname: " << itRed.SiguienteClave() << std::endl;
+		
+		os << "Interfaces: ";
+		Mostrar(os, itRed.SiguienteSignificado().interfaces, '[', ']');
+		os << std::endl;
+		
+		os << "Conexiones: ";
+		typename Dicc<interfaz, hostname>::const_Iterador itConex = itRed.SiguienteSignificado().conexiones.CrearIt();
+		while (itConex.HaySiguiente()){
+			os << '(' << itConex.SiguienteClave() << ',' << itConex.SiguienteSignificado() << ") ";	
+			itConex.Avanzar();		
+		}	
+		os << std::endl;
+
+		os << "Alcanzables: " << std::endl;
+		typename Dicc<hostname, Conj< Lista<hostname> > >::const_Iterador itAlcanz = itRed.SiguienteSignificado().alcanzables.CrearIt();
+		while (itAlcanz.HaySiguiente()){
+			os << itAlcanz.SiguienteClave() << ": " ;
+			Mostrar(os, itAlcanz.SiguienteSignificado(), '{', '}');
+			os << std::endl;
+			itAlcanz.Avanzar();
+		}
+		os << std::endl << std::endl;
+		itRed.Avanzar();
+	}
 }
 
--- tp3/Red/main.cpp
+++ tp3/Red/main.cpp
@@ -4,20 +4,24 @@
 using std::cout;
 using std::endl;
 
+	String ImprimirBool (bool valor){
+		if (valor) return "True";
+		else return "False";
+	}
 
 
 int main(){
 
-	Conj<interfaz> i1 = Conj<interfaz>();
-	i1.AgregarRapido(1);
-	Conj<interfaz> i2 = Conj<interfaz>(i1);
-	i2.AgregarRapido(2);
-	Conj<interfaz> i3 = Conj<interfaz>(i2);
-	i3.AgregarRapido(3);
+	Conj<interfaz> i = Conj<interfaz>();
+	i.AgregarRapido(1);
+	i.AgregarRapido(2);
+	i.AgregarRapido(3);
+	i.AgregarRapido(4);
+	i.AgregarRapido(5);
 
-	computadora compu1 = computadora("compu1", i1);
-	computadora compu2 = computadora("compu2", i2);
-	computadora compu3 = computadora("compu3", i2);
+	computadora compu1 = computadora("compu1", i);
+	computadora compu2 = computadora("compu2", i);
+	computadora compu3 = computadora("compu3", i);
 	
 	Red red = Red();
 	red.AgregarCompu(compu1);
@@ -25,12 +29,43 @@ int main(){
 	red.AgregarCompu(compu3);
 
 	red.Conectar("compu1", 1, "compu2", 1);
+	red.Conectar("compu2", 2, "compu3", 3);
 
+	/*
 	cout << red.UsaInterfaz("compu2", 1) << endl;
 	cout << red.UsaInterfaz("compu2", 2) << endl;
 	cout << red.UsaInterfaz("compu2", 3) << endl;
 	cout << red.UsaInterfaz("compu2", 4) << endl;
 	cout << red.UsaInterfaz("compu2", 1) << endl;
+	*/
+
+	//red.ImprimirRed(cout);
+
+	computadora compu4 = computadora("compu4", i);
+	computadora compu5 = computadora("compu5", i);
+	computadora compu6 = computadora("compu6", i);
+
+	red.AgregarCompu(compu4);
+	red.AgregarCompu(compu5);
+	red.AgregarCompu(compu6);
+
+	red.Conectar("compu1", 2, "compu4", 1);
+	red.Conectar("compu4", 2, "compu3", 1);
+	red.Conectar("compu4", 3, "compu2", 3);
+	red.Conectar("compu5", 1, "compu2", 4);
+	red.Conectar("compu4", 4, "compu5", 2);
+
+	red.Conectar("compu6", 1, "compu3", 2);
+
+	red.ImprimirRed(cout);
+
+
+	cout << "Estan conectadas 6 y 1?: " << ImprimirBool(red.Conectadas("compu6", "compu1")) << endl;
+	cout <<"Hay camino entre 6 y 1?: " << ImprimirBool(red.HayCamino("compu6", "compu1")) << endl;
+	cout << "Caminos minimos entre 6 y 1: " << endl;
+	Mostrar(cout, red.CaminosMinimos("compu6", "compu1"), '[', ']');
+	cout << endl << endl;
+
 
     return 0;
 }
